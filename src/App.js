212952556781307a74086cff6a82a5a9091c7adc
lecovska <lecovska@gmail.com>
2022-08-28 15:34:03 +0200

    fetching data

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
 
 
   const proba = () => {
-    fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+    fetch("https://api.thecatapi.com/v1/images/search")
       .then(res =>res.json())
-      .then(data => console.log(data));
+      .then(data => setNesto(data));
   }
 
 
@@ -20,12 +20,11 @@ function App() {
   // },[]);
 
   return (
-    <div className="App">
+    <div className="app">
 
-
-      <div>{nesto}</div>
-
-
+<img className='smallCat' src='https://static.vecteezy.com/system/resources/previews/002/497/282/large_2x/cute-three-color-cat-waving-paw-cartoon-vector.jpg' alt='cat'></img>
+      <div className='catPic'>{nesto.map(e=><img id="macka" src={e.url}></img>)}</div>
+      <img onClick={proba}  className='paw' src='https://img.freepik.com/premium-vector/cute-hand-drawn-cat-paws-white-background-vector-adorable-animals-silhouette-trendy-style-funny-cute-hygge-illustration-poster-banner-print-decoration-kids-playroom_514409-1359.jpg?w=2000' alt='paw'></img>
 
 
     </div>
